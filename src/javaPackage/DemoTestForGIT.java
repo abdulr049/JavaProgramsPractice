0092395717cdebfcd94c9927d8952b7b5e448886
Abdul Rahaman <abdulr049@gmail.com>
2025-03-21 19:55:58 +0300

    DemoTestForGIT Added

---
+++ src/javaPackage/DemoTestForGIT.java
@@ -0,0 +1,13 @@
+package javaPackage;
+
+public class DemoTestForGIT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("This is Git Demo:");
+		
+	}
+
+}
